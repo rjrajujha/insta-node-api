d728a1b8b1f221b02268a5d5ad3d46c877e27b68
rjrajujha <rjrajujha.com@outlook.com>
2022-12-18 12:40:26 +0530

    api app.js updated

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const userData = require('./routes/user');
 
 app.get('/', (req, res) => {
-  res.send('This Page is under construction');
+  res.send("Page");
   res.end();
 })
+
+// app.use(cors())
+// app.use(express.json());
+// app.use(express.urlencoded())
+// app.use("/api/users",userData);
 
 app.get('/*', (req, res) => {
   res.send('Page Not Found');
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+require('dotenv').config();
 const app = require('./app');
+const mongoose = require('mongoose');
 const port = process.env.PORT || 8080;
+const dbURL = process.env.MONGODB_URI || 'mongodb+srv://root:PassWord@mycloudcluster.glkpt0w.mongodb.net/myProjectDB';
+
+mongoose.connect(dbURL, () => { console.log("Database connection successfull") });
 
 app.listen(port, () => {
   console.log(`Server is up @ port ${port}`)
